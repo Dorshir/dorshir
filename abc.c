a13d32462aae3911c447023d5fa3c35f024ff43f
Dor Shir <117517821+Dorshir@users.noreply.github.com>
2024-10-12 17:34:38 +0000

    abc completed

--- abc.c
+++ abc.c
@@ -1,4 +1,5 @@
 #include "abc.h"
+#define SET_SIZE 256
 
 int CheckInputStrStr(char *str1, char *str2)
 {
@@ -14,16 +15,19 @@ int IsEntireStr2InSubStr1(char *str1, char *str2, int *index1, size_t length1, s
 {
     int result = FALSE;
     int index2 = 0;
+    int i = *index1;
 
-    while (*index1 < length1 && index2 < length2 && str2[index2] == str1[*index1])
+    while (i < length1 && index2 < length2 && str2[index2] == str1[i])
     {
         index2++;
-        *index1 = *index1 + 1;
+        i++;
     }
 
+    /* If entire str2 from at position *index1 = i, return TRUE and update index1, else index1 wont be incremented */
     if (index2 == length2)
     {
         result = TRUE;
+        *index1 = i;
     }
 
     return result;
@@ -82,7 +86,7 @@ void PrintBinary(int *array, int *index)
     printf("\n");
 }
 
-void StringDecToBinary(char *str)
+int StringDecToBinary(char *str)
 {
     int index;
     size_t length;
@@ -91,15 +95,60 @@ void StringDecToBinary(char *str)
 
     if (str == NULL)
     {
-        return;
+        return NULL_PTR_ERROR;
     }
 
     length = strlen(str);
     index = 0;
 
-    number = atoi(str);
-    ConvertToBinary(number, binaryArray, &index);
-    PrintBinary(binaryArray, &index);
+    if (length == 1 && str[0] == '0')
+    {
+        printf("0\n");
+    }
+    /* Empty or invalid input (e.g abc123) */
+    else if (length == 0 || ((number = atoi(str)) == 0))
+    {
+        return INVALID_INPUT_ERROR;
+    }
+    /* Valid input, convert to binary representation and print */
+    else
+    {
+        ConvertToBinary(number, binaryArray, &index);
+        PrintBinary(binaryArray, &index);
+    }
+    return OK;
 }
 
+void MarkPresented(char charSet[], const char *str)
+{
+    int index;
+    for (index = 0; str[index] != '\0'; index++)
+    {
+        charSet[(unsigned char)str[index]] = TRUE;
+    }
+}
 
+int Squeeze(char *s1, const char *s2)
+{
+    char charsPresented[SET_SIZE] = {0};
+    int left;
+    int right;
+
+    if (s1 == NULL || s2 == NULL)
+    {
+        return NULL_PTR_ERROR;
+    }
+
+    MarkPresented(charsPresented, s2);
+
+    for (left = 0, right = 0; s1[right] != '\0'; right++)
+    {
+        if (!charsPresented[(unsigned char)s1[right]])
+        {
+            s1[left++] = s1[right];
+        }
+    }
+    s1[left] = '\0';
+
+    return OK;
+}
--- abc.h
+++ abc.h
@@ -3,16 +3,39 @@
 #include <math.h>
 #define TRUE 1
 #define FALSE 0
+#define NULL_PTR_ERROR -1
+#define OK 1
+#define INVALID_INPUT_ERROR -2
+
 
 /**
  * @brief   Finds the first occurrence of the substring str2 in the string str1.
  *
  * @param   str1    The string which we are looking for the substring str2 in it.
  * @param   str2    The string that we are looking for in str2.
- * @return  A pointer to the beginning of the located substring in str1, or NULL if the substring is not found.
+ * @return  Returns a pointer to the beginning of the located substring in str1 if exist.
+ *          Returns NULL if the substring is not found.
  */
 void *MyStrStr(char *str1, char *str2);
 
-void StringDecToBinary(char* str);
+/**
+ * @brief   Converts a decimal number provided as a string to its binary representation and prints it.
+ *
+ * @param   str     The string containing the decimal number to convert.
+ *
+ * @return  Returns OK (0) on success.
+ *          Returns NULL_PTR_ERROR (-1) if the input string pointer is NULL.
+ *          Returns INVALID_INPUT_ERROR (-2) if the input is invalid (e.g., empty string or non-numeric).
+ */
+int StringDecToBinary(char* str);
 
-void StringBinaryToDec(char* str);
+/**
+ * @brief   Removes all characters from the string s1 that match any character in the string s2.
+ *
+ * @param   s1  The string to be modified. Characters found in s2 will be removed from this string.
+ * @param   s2  The string containing characters to be removed from s1.
+ *
+ * @return  Returns OK (0) on success.
+ *          Returns NULL_PTR_ERROR (-1) if either s1 or s2 is NULL.
+ */
+int Squeeze(char* s1, const char* s2);
