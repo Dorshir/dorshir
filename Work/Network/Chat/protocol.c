a392088436e33f23b8a24d87098dd0c726a9fdca
Dor Shir <117517821+Dorshir@users.noreply.github.com>
2024-12-12 17:11:34 +0000

    protocol get groups

--- Work/Network/Chat/protocol.c
+++ Work/Network/Chat/protocol.c
@@ -9,6 +9,13 @@
 #define USER_NAME_PASS_RES_SIZE_TL 3
 #define USER_NAME_STATUS_SIZE 1
 
+#define GROUP_REQ_SIZE_TL 4
+#define GROUP_RES_SIZE_TL 3
+#define GROUP_STATUS_SIZE 1
+
+#define GET_GROUPS_REQ_SIZE_TL 3
+#define GET_GROUPS_RES_SIZE_TL 4
+
 #define TAG_POS 0
 #define TOTAL_LENGTH_POS 1
 #define FIRST_FIELD_LEN_POS 2
@@ -18,6 +25,13 @@ static size_t PackUserNamePassReq(ProtocolTag _tag, UserNamePass *_userNamePass,
 static ProtocolResult UnPackUserNamePassReq(char *_packed, size_t _length, UserNamePass *_userNamePass);
 static size_t PackUserNamePassRes(ProtocolTag _tag, char _status, char *_buffer);
 static ProtocolResult UnPackUserNamePassRes(char *_buffer, char *_status);
+static size_t PackGroupReq(ProtocolTag _tag, GroupRequest *_groupReq, char *_packed);
+static ProtocolResult UnPackGroupReq(char *_packed, size_t _length, GroupRequest *_groupReq);
+static size_t PackGroupResWithAddress(ProtocolTag _tag, char _status, const char *_address, char *_buffer);
+static size_t PackGetGroupsReqWrapper(ProtocolTag _tag, char *_userName, char *_packed);
+static ProtocolResult UnPackGetGroupsReqWrapper(ProtocolTag _tag, char *_packed, size_t _length, char *_userName);
+static size_t PackGetGroupsResWrapper(ProtocolTag _tag, char _status, char *_groupsList, char *_packed);
+static ProtocolResult UnPackCreateGroupResWrapper(ProtocolTag _tag, char *_packed, size_t _length, char *_status, char *_groupsList);
 
 /* Registration Request */
 
@@ -117,6 +131,150 @@ ProtocolResult UnPackLoginRes(char *_buffer, size_t _length, char *_status)
     return UnPackUserNamePassRes(_buffer, _status);
 }
 
+/* Create Group Request */
+
+size_t PackCreateGroupReq(GroupRequest *_groupReq, char *_packed)
+{
+    if (_groupReq == NULL || _packed == NULL)
+    {
+        return 0;
+    }
+    return PackGroupReq(PROTOCOL_CREATE_GROUP_REQ, _groupReq, _packed);
+}
+
+ProtocolResult UnPackCreateGroupReq(char *_packed, size_t _length, GroupRequest *_groupReq)
+{
+    if (_groupReq == NULL || _length == 0 || _packed == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    if (_packed[TAG_POS] != PROTOCOL_CREATE_GROUP_REQ)
+    {
+        return PROTOCOL_UNPACK_FAILURE;
+    }
+    return UnPackGroupReq(_packed, _length, _groupReq);
+}
+
+/* Create Group Response */
+
+size_t PackCreateGroupResWithAddress(char _status, const char *_address, char *_buffer)
+{
+    return PackGroupResWithAddress(PROTOCOL_CREATE_GROUP_RES, _status, _address, _buffer);
+}
+
+ProtocolResult UnPackCreateGroupResWithAddress(char *_buffer, size_t _length, CreateGroupResponse *_response)
+{
+    return UnPackGroupResWithAddress(_buffer, _length, &_response->m_status, &_response->m_address, PROTOCOL_CREATE_GROUP_RES);
+}
+
+/* Get Groups Request */
+
+size_t PackGetGroupsReq(char *_userName, char *_packed)
+{
+    return PackGetGroupsReqWrapper(PROTOCOL_GET_GROUPS_REQ, _userName, _packed);
+}
+
+ProtocolResult UnPackGetGroupsReq(char *_packed, size_t _length, char *_userName)
+{
+    return UnPackGetGroupsReqWrapper(PROTOCOL_GET_GROUPS_REQ, _packed, _length, _userName);
+}
+
+/* Get Groups Response */
+
+size_t PackGetGroupsRes(char _status, char *_groupsList, char *_packed)
+{
+    return PackGetGroupsResWrapper(PROTOCOL_GET_GROUPS_RES, _status, _groupsList, _packed);
+}
+
+ProtocolResult UnPackCreateGroupRes(char *_packed, size_t _length, char *_status, char *_groupsList)
+{
+    return UnPackCreateGroupResWrapper(PROTOCOL_GET_GROUPS_RES, _packed, _length, _status, _groupsList);
+}
+
+/* Join Group Request*/
+
+size_t PackJoinGroupReq(GroupRequest *_groupReq, char *_packed)
+{
+    if (_groupReq == NULL || _packed == NULL)
+    {
+        return 0;
+    }
+    return PackGroupReq(PROTOCOL_JOIN_GROUP_REQ, _groupReq, _packed);
+}
+
+ProtocolResult UnPackJoinGroupReq(char *_packed, size_t _length, GroupRequest *_groupReq)
+{
+    if (_groupReq == NULL || _length == 0 || _packed == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    if (_packed[TAG_POS] != PROTOCOL_JOIN_GROUP_REQ)
+    {
+        return PROTOCOL_UNPACK_FAILURE;
+    }
+    return UnPackGroupReq(_packed, _length, _groupReq);
+}
+
+/* Join Group Response */
+
+size_t PackJoinGroupResWithAddress(char _status, const char *_address, char *_buffer)
+{
+    return PackGroupResWithAddress(PROTOCOL_JOIN_GROUP_RES, _status, _address, _buffer);
+}
+
+ProtocolResult UnPackJoinGroupResWithAddress(char *_buffer, size_t _length, JoinGroupResponse *_response)
+{
+    return UnPackGroupResWithAddress(_buffer, _length, &_response->m_status, &_response->m_address, PROTOCOL_JOIN_GROUP_RES);
+}
+
+/* Leave Group Request */
+
+size_t PackLeaveGroupReq(GroupRequest *_groupReq, char *_packed)
+{
+    if (_groupReq == NULL || _packed == NULL)
+    {
+        return 0;
+    }
+    return PackGroupReq(PROTOCOL_LEAVE_GROUP_REQ, _groupReq, _packed);
+}
+
+ProtocolResult UnPackLeaveGroupReq(char *_packed, size_t _length, GroupRequest *_groupReq)
+{
+    if (_groupReq == NULL || _length == 0 || _packed == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    if (_packed[TAG_POS] != PROTOCOL_LEAVE_GROUP_REQ)
+    {
+        return PROTOCOL_UNPACK_FAILURE;
+    }
+    return UnPackGroupReq(_packed, _length, _groupReq);
+}
+
+/* Leave Group Response */
+
+size_t PackLeaveGroupRes(char _status, char *_buffer)
+{
+    if (_buffer == NULL)
+    {
+        return 0;
+    }
+    return PackUserNamePassRes(PROTOCOL_LEAVE_GROUP_RES, _status, _buffer);
+}
+
+ProtocolResult UnPackLeaveGroupRes(char *_buffer, size_t _length, char *_status)
+{
+    if (_status == NULL || _length < GROUP_RES_SIZE_TL + GROUP_STATUS_SIZE || _buffer == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    if (_buffer[TAG_POS] != PROTOCOL_LEAVE_GROUP_RES)
+    {
+        return PROTOCOL_UNPACK_FAILURE;
+    }
+    return UnPackUserNamePassRes(_buffer, _status);
+}
+
 /*****************************************************************************************************************/
 
 ProtocolTag GetTag(char *_buffer)
@@ -128,7 +286,7 @@ ProtocolTag GetTag(char *_buffer)
 
     ProtocolTag tag = _buffer[TAG_POS];
 
-    if (tag < PROTOCOL_REGISTRATION_REQ || tag > PROTOCOL_LOGIN_RES)
+    if (tag < PROTOCOL_REGISTRATION_REQ || tag > PROTOCOL_LEAVE_GROUP_RES)
     {
         return PROTOCOL_INVALID_TAG;
     }
@@ -192,16 +350,208 @@ static ProtocolResult UnPackUserNamePassReq(char *_packed, size_t _length, UserN
 
 static size_t PackUserNamePassRes(ProtocolTag _tag, char _status, char *_buffer)
 {
-    _buffer[TAG_POS] = PROTOCOL_REGISTRATION_RES;
+    _buffer[TAG_POS] = _tag;
     _buffer[TOTAL_LENGTH_POS] = USER_NAME_PASS_RES_SIZE_TL + USER_NAME_STATUS_SIZE;
     _buffer[FIRST_FIELD_LEN_POS] = USER_NAME_STATUS_SIZE;
     _buffer[FIRST_FIELD_POS] = _status;
-
     return USER_NAME_PASS_RES_SIZE_TL + USER_NAME_STATUS_SIZE;
 }
 
 static ProtocolResult UnPackUserNamePassRes(char *_buffer, char *_status)
 {
     *_status = _buffer[FIRST_FIELD_POS];
+    return PROTOCOL_SUCCESS;
+}
+
+static size_t PackGroupReq(ProtocolTag _tag, GroupRequest *_groupReq, char *_packed)
+{
+    size_t userNameLen = strlen(_groupReq->m_userName);
+    size_t groupNameLen = strlen(_groupReq->m_groupName);
+    size_t length = GROUP_REQ_SIZE_TL + userNameLen + groupNameLen;
+
+    if (length >= MAX_MSG_SIZE)
+    {
+        return 0;
+    }
+
+    _packed[TAG_POS] = (char)_tag;
+    _packed[TOTAL_LENGTH_POS] = (char)length;
+    _packed[FIRST_FIELD_LEN_POS] = (char)userNameLen;
+    memcpy(&_packed[FIRST_FIELD_POS], _groupReq->m_userName, userNameLen);
+    _packed[FIRST_FIELD_POS + userNameLen] = (char)groupNameLen;
+    memcpy(&_packed[userNameLen + GROUP_REQ_SIZE_TL], _groupReq->m_groupName, groupNameLen);
+
+    return length;
+}
+
+static ProtocolResult UnPackGroupReq(char *_packed, size_t _length, GroupRequest *_groupReq)
+{
+    if (_length < GROUP_REQ_SIZE_TL)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    unsigned char userNameLen = (unsigned char)_packed[FIRST_FIELD_LEN_POS];
+    if (GROUP_REQ_SIZE_TL + userNameLen > _length)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    unsigned char groupNameLen = (unsigned char)_packed[FIRST_FIELD_POS + userNameLen];
+    if (GROUP_REQ_SIZE_TL + userNameLen + groupNameLen > _length)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    memcpy(_groupReq->m_userName, &_packed[FIRST_FIELD_POS], userNameLen);
+    memcpy(_groupReq->m_groupName, &_packed[FIRST_FIELD_POS + userNameLen + 1], groupNameLen);
+
+    _groupReq->m_userName[userNameLen] = '\0';
+    _groupReq->m_groupName[groupNameLen] = '\0';
+
+    return PROTOCOL_SUCCESS;
+}
+
+static size_t PackGroupResWithAddress(ProtocolTag _tag, char _status, const char *_address, char *_buffer)
+{
+    size_t addrLen;
+    if (_buffer == NULL || (_status == 1 && _address == NULL))
+    {
+        return 0;
+    }
+    _buffer[TAG_POS] = _tag;
+    if (_status == 0)
+    {
+        _buffer[TOTAL_LENGTH_POS] = GROUP_RES_SIZE_TL;
+        _buffer[FIRST_FIELD_LEN_POS] = GROUP_STATUS_SIZE;
+        _buffer[FIRST_FIELD_POS] = _status;
+        return GROUP_RES_SIZE_TL;
+    }
+    addrLen = strlen(_address);
+    _buffer[TOTAL_LENGTH_POS] = GROUP_RES_SIZE_TL + addrLen;
+    _buffer[FIRST_FIELD_LEN_POS] = GROUP_STATUS_SIZE;
+    _buffer[FIRST_FIELD_POS] = _status;
+    _buffer[FIRST_FIELD_POS + 1] = (char)addrLen;
+    memcpy(&_buffer[FIRST_FIELD_POS + 2], _address, addrLen);
+    return GROUP_RES_SIZE_TL + addrLen + 2;
+}
+
+static ProtocolResult UnPackGroupResWithAddress(char *_buffer, size_t _length, char *_status, char **_address, ProtocolTag _expectedTag)
+{
+    unsigned char addrLen;
+
+    if (_buffer == NULL || _length < GROUP_RES_SIZE_TL || _status == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    if (_buffer[TAG_POS] != _expectedTag)
+    {
+        return PROTOCOL_UNPACK_FAILURE;
+    }
+    *_status = _buffer[FIRST_FIELD_POS];
+
+    if (*_status == 0)
+    {
+        *_address = NULL;
+        return PROTOCOL_SUCCESS;
+    }
+    addrLen = (unsigned char)_buffer[FIRST_FIELD_POS + 1];
+    if (_length < GROUP_RES_SIZE_TL + addrLen)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    *_address = (char *)malloc(addrLen + 1);
+    if (*_address == NULL)
+    {
+        return PROTOCOL_UNINITIALIZED_ERROR;
+    }
+    memcpy(*_address, &_buffer[FIRST_FIELD_POS + 2], addrLen);
+    (*_address)[addrLen] = '\0';
+
+    return PROTOCOL_SUCCESS;
+}
+
+static size_t PackGetGroupsReqWrapper(ProtocolTag _tag, char *_userName, char *_packed)
+{
+    size_t userNameLen = strlen(_userName);
+
+    size_t length = GET_GROUPS_REQ_SIZE_TL + userNameLen;
+
+    if (length >= MAX_MSG_SIZE)
+    {
+        return 0;
+    }
+
+    _packed[TAG_POS] = (char)_tag;
+    _packed[TOTAL_LENGTH_POS] = (char)length;
+    _packed[FIRST_FIELD_LEN_POS] = (char)userNameLen;
+    memcpy(&_packed[FIRST_FIELD_POS], _userName, userNameLen);
+
+    return length;
+}
+
+static ProtocolResult UnPackGetGroupsReqWrapper(ProtocolTag _tag, char *_packed, size_t _length, char *_userName)
+{
+    if (_length < GET_GROUPS_REQ_SIZE_TL)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    unsigned char userNameLen = (unsigned char)_packed[FIRST_FIELD_LEN_POS];
+
+    if (USER_NAME_PASS_REQ_SIZE_TL + userNameLen > _length)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    memcpy(_userName, &_packed[FIRST_FIELD_POS], userNameLen);
+
+    _userName[userNameLen] = '\0';
+
+    return PROTOCOL_SUCCESS;
+}
+
+static size_t PackGetGroupsResWrapper(ProtocolTag _tag, char _status, char *_groupsList, char *_packed)
+{
+    size_t groupsListLen = strlen(_groupsList);
+
+    size_t length = GET_GROUPS_RES_SIZE_TL + groupsListLen;
+
+    if (length >= MAX_MSG_SIZE)
+    {
+        return 0;
+    }
+
+    _packed[TAG_POS] = (char)_tag;
+    _packed[TOTAL_LENGTH_POS] = (char)length;
+    _packed[FIRST_FIELD_LEN_POS] = GROUP_STATUS_SIZE;
+    _packed[FIRST_FIELD_POS] = _status;
+    _packed[FIRST_FIELD_POS + GROUP_STATUS_SIZE] = (char)groupsListLen;
+    memcpy(&_packed[GROUP_STATUS_SIZE + GET_GROUPS_RES_SIZE_TL], _groupsList, groupsListLen);
+
+    return length;
+}
+
+static ProtocolResult UnPackCreateGroupResWrapper(ProtocolTag _tag, char *_packed, size_t _length, char *_status, char *_groupsList)
+{
+    if (_length < GET_GROUPS_RES_SIZE_TL)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    unsigned char groupsListLen = (unsigned char)_packed[FIRST_FIELD_POS + GROUP_STATUS_SIZE];
+
+    if (GET_GROUPS_RES_SIZE_TL + groupsListLen > _length)
+    {
+        return PROTOCOL_INVALID_BUFFER_LENGTH;
+    }
+
+    *_status = _packed[FIRST_FIELD_POS];
+
+    memcpy(_groupsList, &_packed[FIRST_FIELD_POS + GROUP_STATUS_SIZE + 1], groupsListLen);
+
+    _groupsList[groupsListLen] = '\0';
+
     return PROTOCOL_SUCCESS;
 }
--- Work/Network/Chat/protocol.h
+++ Work/Network/Chat/protocol.h
@@ -2,7 +2,6 @@
 #define __PROTOCOL_H__
 
 #include <stddef.h> /* size_t */
-#include <stddef.h> /* size_t */
 
 typedef struct Protocol Protocol;
 
@@ -22,6 +21,14 @@ typedef enum Protocol_Tag
     PROTOCOL_REGISTRATION_RES,
     PROTOCOL_LOGIN_REQ,
     PROTOCOL_LOGIN_RES,
+    PROTOCOL_CREATE_GROUP_REQ,   
+    PROTOCOL_CREATE_GROUP_RES,   
+    PROTOCOL_JOIN_GROUP_REQ,     
+    PROTOCOL_JOIN_GROUP_RES,
+    PROTOCOL_GET_GROUPS_REQ,
+    PROTOCOL_GET_GROUPS_RES, 
+    PROTOCOL_LEAVE_GROUP_REQ,    
+    PROTOCOL_LEAVE_GROUP_RES
 
 } ProtocolTag;
 
@@ -31,7 +38,26 @@ typedef struct UserNamePass
     char *m_password;
 } UserNamePass;
 
+typedef struct GroupRequest
+{
+    char* m_userName;
+    char* m_groupName;
+} GroupRequest;
+
+typedef struct CreateGroupResponse
+{
+    char m_status;
+    char* m_address;
+} CreateGroupResponse;
+
+typedef struct JoinGroupResponse
+{
+    char m_status;
+    char* m_address;
+} JoinGroupResponse;
+
 ProtocolTag GetTag(char *_buffer);
+
 
 /* Registration */
 
@@ -43,6 +69,7 @@ size_t PackRegRes(char _status, char *_buffer);
 
 ProtocolResult UnPackRegRes(char *_buffer, size_t _length, char *_status);
 
+
 /* Login */
 
 size_t PackLoginReq(UserNamePass *_userNamePass, char *_packed);
@@ -53,5 +80,48 @@ size_t PackLoginRes(char _status, char *_buffer);
 
 ProtocolResult UnPackLoginRes(char *_buffer, size_t _length, char *_status);
 
+
+/* Create Group */
+
+size_t PackCreateGroupReq(GroupRequest* _groupReq, char* _packed);
+
+ProtocolResult UnPackCreateGroupReq(char* _packed, size_t _length, GroupRequest* _groupReq);
+
+size_t PackCreateGroupResWithAddress(char status, const char* address, char* buffer);
+
+ProtocolResult UnPackCreateGroupResWithAddress(char* buffer, size_t length, CreateGroupResponse* response);
+
+
+/* Join Group */
+
+size_t PackJoinGroupReq(GroupRequest* _groupReq, char* _packed);
+
+ProtocolResult UnPackJoinGroupReq(char* _packed, size_t _length, GroupRequest* _groupReq);
+
+size_t PackJoinGroupResWithAddress(char status, const char* address, char* buffer);
+
+ProtocolResult UnPackJoinGroupResWithAddress(char* buffer, size_t length, JoinGroupResponse* response);
+
+
+/* Get Groups */
+
+size_t PackGetGroupsReq(char *_userName, char *_packed);
+
+ProtocolResult UnPackGetGroupsReq(char *_packed, size_t _length, char *_userName);
+
+size_t PackGetGroupsRes(char _status, char *_groupsList, char *_packed);
+
+ProtocolResult UnPackCreateGroupRes(char *_packed, size_t _length, char *_status, char *_groupsList);
+
+
+/* Leave Group */
+
+size_t PackLeaveGroupReq(GroupRequest* _groupReq, char* _packed);
+
+ProtocolResult UnPackLeaveGroupReq(char* _packed, size_t _length, GroupRequest* _groupReq);
+
+size_t PackLeaveGroupRes(char _status, char* _buffer);
+
+ProtocolResult UnPackLeaveGroupRes(char* _buffer, size_t _length, char* _status);
 
 #endif /* __PROTOCOL_H__ */
